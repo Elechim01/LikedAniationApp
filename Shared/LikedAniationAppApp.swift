5503c4f0f73a801c3d9b0d14f3240855cb1d2747
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-09-10 18:10:19 +0200

    Initial Commit

---
+++ Shared/LikedAniationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LikedAniationAppApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 10/09/21.
+//
+
+import SwiftUI
+
+@main
+struct LikedAniationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
